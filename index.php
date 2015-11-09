dd99ee51eea5aaece2bc49989428bc1f2bb57c5f
Fatimah <fatimah_o@hotmail.com>
2015-11-09 20:03:18 +0000

    index rename

--- index.php
+++ index.php
@@ -7,7 +7,12 @@
 <p>
     <?php
     $round = round(M_PI);
-    print$round;
+    print$round;  // prints 3
+
+    // This time, round pi to 4 places
+    $round_decimal = round(M_PI, 4);
+    print$round_decimal; // prints 3.1416
+
 
 
     ?>
