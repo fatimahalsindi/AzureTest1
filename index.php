6c76321ed8ba812d93a61db2985b8c2b7bc9a212
Fatimah <fatimah_o@hotmail.com>
2015-11-09 15:12:08 +0000

    index rename

--- index.php
+++ index.php
@@ -6,21 +6,10 @@
 <body>
 <p>
     <?php
-    $wantedgood = "mugs";
+    $myArray = array("do", "re", "mi"); // declares the array
 
-    switch ($wantedgood) {
-        case  "specs":
-            echo "You have to be 16 to buy specs";
-            break;
-        case  "mugs":
-            echo "You have to be 18 to buy mugs";
-            break;
-        case  "sausage rolls":
-            echo "You have to be 21 to buy sausage rolls";
-            break;
-        default:
-            echo "You haven't selected a thing to buy";
-    }
+    echo$myArray[0]
+
 
 
     ?>
