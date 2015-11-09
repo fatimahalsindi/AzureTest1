9c3ccde68a85156686210a399984b78211af92ab
Fatimah <fatimah_o@hotmail.com>
2015-11-09 16:15:02 +0000

    index rename

--- index.php
+++ index.php
@@ -6,19 +6,21 @@
 <body>
 <p>
     <?php
-    $myArray = array("specs", "mugs", "sausage rolls");
+   /* $myArray = array("specs", "mugs", "sausage rolls");
 
     print $myArray;
 
     $myArray [1] = "hugs"; // modifies position 1 (re)
 
-    print $myArray(1);
+    print $myArray;
 
     unset($array [2]); // removes the array in position 2
 
-    print $myArray;
+    print $myArray;*/
 
+    $myArray = array("specs", "mugs", "sausage rolls");
 
+    print $myArray[1];
 
     ?>
 </p>
