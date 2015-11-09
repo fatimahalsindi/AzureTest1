f6d47519d3c17e97a586ae9ab6c9a52ae2efbe4d
Fatimah <fatimah_o@hotmail.com>
2015-11-09 17:53:31 +0000

    index rename

--- index.php
+++ index.php
@@ -6,25 +6,34 @@
 <body>
 <p>
     <?php
-   $myArray = array("specs", "mugs", "sausage rolls");
-
-    printmyArray($myArray);
-
-    $myArray [1] = "hugs"; // modifies position 1 (re)
-
-    printmyArray($myArray);
-
-    unset($myArray [2]); // removes the array in position 2
-
-    printmyArray($myArray);
-
-
-    function printmyArray($myArray)
+    for ($i = 1; $i < 30; $i++)
     {
-        foreach($myArray as $x) {
-            echo "<p>" . $x . "</p>";
+        $y = 0; //I'm using this as a marker to tell that at least one thing is being made
+        echo "<p>On day " . $i . " the following products are available: ";
+
+        if ($i % 2 != 0)
+        {
+            echo "Sausage Rolls ";
+            $y = 1;
         }
+        if ($i % 3 != 0)
+        {
+            echo "Mugs ";
+            $y = 1;
+        }
+        if ($i % 4 != 0)
+        {
+            echo "Specs";
+            $y = 1;
+        }
+        if ($y == 1) //if one thing isn't being made on this day then it displays that nothing is available that day.
+        {
+            echo "NONE";
+        }
+        echo "</p>";
     }
+
+
 
 
     ?>
