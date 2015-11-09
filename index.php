f914d876dd937722cc0422bddaad3d0bf6ea4d12
Fatimah <fatimah_o@hotmail.com>
2015-11-09 18:54:02 +0000

    index rename

--- index.php
+++ index.php
@@ -5,35 +5,12 @@
 </head>
 <body>
 <p>
+
     <?php
-
-    for ($b = 1; $b < 30; $b++)
-    {
-        $y = 0; //I'm using this as a marker to tell that at least one thing is being made
-        echo "<p>On day " . $b. " the following products are available: ";
-
-        if ($b % 2 b= 0)
-        {
-            echo "Sausage Rolls ";
-            $y = 1;
-        }
-        if ($b % 3 b= 0)
-        {
-            echo "Mugs ";
-            $y = 1;
-        }
-        if ($b % 4 b= 0)
-        {
-            echo "Specs";
-            $y = 1;
-        }
-        if ($y == 1) //if one thing isn't being made on this day then it displays that nothing is available that day.
-        {
-            echo "NONE";
-        }
-        echo "</p>";
-    }
-
+// get the length of a string and
+// print it to the screen
+$length = strlen("david");
+print$length;
 
 
     ?>
